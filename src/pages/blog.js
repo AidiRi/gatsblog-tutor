8ca78254f88baa7a9b6f11a9aca3c163f6733de7
AidiRi <rivera.aidi@gmail.com>
2020-01-02 14:37:50 -0800

    BlogPage shell added.

---
+++ src/pages/blog.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const BlogPage = () =>{
+  return(
+    <div>
+      <h1>Blog</h1>
+      <p>Posts will show up here tomorrow-ish.</p>
+    </div>
+  )
+}
+
+export default BlogPage
