e672b35fc4a9003f05e3b176a227cb89c7e46ec6
AidiRi <rivera.aidi@gmail.com>
2020-01-02 15:06:36 -0800

    ContactPage shell added.

---
+++ src/pages/contact.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ContactPage = () => {
+  return(
+    <div>
+      <h1>Get In Touch</h1>
+      <address>
+        <p>Email me at: Aidi@gmail.com</p>
+      </address>
+    </div>
+  )
+}
+
+export default ContactPage
