53eb4428954947c72a4f1cd4a66cd87cf9bb55ca
AidiRi <rivera.aidi@gmail.com>
2020-01-03 15:16:20 -0800

    Created Header and added to pages.

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import Header from '../components/header'
 import Footer from '../components/footer'
+
 
 const ContactPage = () => {
   return(
     <div>
+      <Header/>
+      
       <h1>Get In Touch</h1>
       <p>Email me at: Aidi@gmail.com</p>
       <p>Or reach me via <a href="https://twitter.com/RiveraAidi" target="_blank">@RiveraAidi</a> on twitter.</p>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import Header from '../components/header'
 import Footer from '../components/footer'
 
 const IndexPage = () =>{
   return(
     <div>
+      <Header/>
+
       <h1>Hello, there.</h1>
-      <h2>I'm Aidiris, a full-stack developer solving first-world problems.</h2>
+      <h2>I'm Aidi, a full-stack developer solving first-world problems.</h2>
       <p>Need a first-world-problem solver? <Link to="/contact">Contact me.</Link></p>
 
       <Footer/>
